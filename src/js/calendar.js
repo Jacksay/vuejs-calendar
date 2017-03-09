08099de2dcc7c99a80a08dd2b87724ca8507d7e0
Stéphane Bouvry <stephane.bouvry@unicaen.fr>
2017-03-09 08:58:05 +0100

    Prototype

--- src/js/calendar.js
+++ src/js/calendar.js
@@ -2,72 +2,80 @@ import EventDT from "EventDT";
 import moment from "moment";
 
 var TimeEvent = {
-  template: `<div class="event" :style="css">
-    <div class="label" data-uid="UID">
-      {{ event.label }}
-    </div>
-    <div class="description">
-      {{ event.description }}
-    </div>
-    <time class="time start">{{ dateStart | hour }}</time>
-    <time class="time end">{{ dateEnd | hour }}</time>
-    <nav>
-      <a @click="deleteEvent">Supprimer</a>
-      <a @click="editEvent">Modifier</a>
-    </nav>
-  </div>`,
-  props: ['event', 'weekDayRef'],
-  data(){
-    return {
-      selected: false
+
+    template: `<div class="event" :style="css">
+        <div class="label" data-uid="UID">
+          {{ event.label }}
+        </div>
+        <div class="description">
+          {{ event.description }}
+        </div>
+        <time class="time start">{{ dateStart | hour }}</time>
+        <time class="time end">{{ dateEnd | hour }}</time>
+        <nav>
+          <a @click="deleteEvent">Supprimer</a>
+          <a @click="editEvent">Modifier</a>
+        </nav>
+      </div>`,
+
+    props: ['event', 'weekDayRef'],
+
+    data(){
+        return {
+            selected: false
+        }
+    },
+
+    filters: {
+        hour(mm){
+            return mm.format('H:mm')
+        },
+        dateFull(mm){
+            return mm.format('D MMMM YYYY, h:mm')
+        }
+    },
+
+    computed: {
+        css(){
+            return {
+                background: 'rgba(255,255,255,.5)',
+                height: (this.pixelEnd - this.pixelStart) + 'px',
+                position: "absolute",
+                top: this.pixelStart + 'px',
+                width: (100 / 7) + "%",
+                left: (this.weekDay * 100 / 7) + "%"
+            }
+        },
+
+        dateStart(){
+            return moment(this.event.start);
+        },
+
+        dateEnd(){
+            return moment(this.event.end);
+        },
+
+        pixelStart(){
+            return this.dateStart.hour() * 40 + (40 / 60 * this.dateStart.minutes());
+        },
+
+        pixelEnd(){
+            return this.dateEnd.hour() * 40 + (40 / 60 * this.dateEnd.minutes());
+        },
+
+        weekDay(){
+            return this.dateStart.day()
+        }
+    },
+
+    mounted(){
+        console.log(this, this.dateStart);
     }
-  },
-
-  filters: {
-      hour(mm){
-        return mm.format('H:mm')
-      },
-      dateFull(mm){
-        return mm.format('D MMMM YYYY, h:mm')
-      }
-  },
-
-  computed: {
-      css(){
-          return {
-            background: 'rgba(255,255,255,.5)',
-            height: (this.pixelEnd - this.pixelStart) +'px',
-            position: "absolute",
-            top: this.pixelStart+'px',
-            width: (100/7)+"%",
-            left: (this.weekDay * 100 / 7)+"%"
-          }
-      },
-      dateStart(){
-          return moment(this.event.start);
-      },
-      dateEnd(){
-          return moment(this.event.end);
-      },
-      pixelStart(){
-        return this.dateStart.hour() * 40 + (40/60*this.dateStart.minutes());
-      },
-      pixelEnd(){
-        return this.dateEnd.hour() * 40 + (40/60*this.dateEnd.minutes());
-      },
-      weekDay(){
-        return this.dateStart.day()
-      }
-  },
-
-  mounted(){
-    console.log(this, this.dateStart);
-  }
 };
 
 var Calendar = {
 
-  template: `<div class="calendar">
+    template: `<div class="calendar">
     <div class="meta">
       {{ currentMonth }}
     </div>
@@ -131,48 +139,48 @@ var Calendar = {
     </footer>
     </div>`,
 
-  props: {
-      mode: 'week'
-  },
+    props: {
+        mode: 'week'
+    },
 
-  data(){
-    return {
-      events: [],
-      currentDay: moment()
+    data(){
+        return {
+            events: [],
+            currentDay: moment()
+        }
+    },
+
+    computed: {
+        currentYear(){
+            return this.currentDay.format('YYYY')
+        },
+        currentMonth(){
+            return this.currentDay.format('MMMM')
+        },
+        currentWeekKey(){
+            return this.currentDay.format('YYYY-W')
+        }
+    },
+
+    components: {
+        'timeevent': TimeEvent
+    },
+
+    methods: {
+        newEvent(evt){
+            console.log("CreateEvent")
+            this.events.push(evt)
+        },
+        inCurrentWeek(event){
+            return event.inWeek(this.currentDay.year(), this.currentDay.week());
+        }
+    },
+
+    mounted(){
+        console.log(this, this.$el);
+        this.newEvent(new EventDT(1, 'Item A', "2017-03-06T09:00", "2017-03-06T11:00"));
+        this.newEvent(new EventDT(4, 'Item D', "2017-03-06T13:30", "2017-03-06T17:45"));
+        this.newEvent(new EventDT(2, 'Item B', "2017-03-08T13:00", "2017-03-08T17:00"));
+        this.newEvent(new EventDT(3, 'Item C', "2017-03-18T09:00", "2017-03-18T11:00"));
     }
-  },
-
-  computed: {
-      currentYear(){
-        return this.currentDay.format('YYYY')
-      },
-      currentMonth(){
-        return this.currentDay.format('MMMM')
-      },
-      currentWeekKey(){
-        return this.currentDay.format('YYYY-W')
-      }
-  },
-
-  components: {
-    'timeevent': TimeEvent
-  },
-
-  methods: {
-      newEvent(evt){
-        console.log("CreateEvent")
-        this.events.push(evt)
-      },
-      inCurrentWeek( event ){
-        return event.inWeek(this.currentDay.year(), this.currentDay.week());
-      }
-  },
-
-  mounted(){
-    console.log(this, this.$el);
-    this.newEvent( new EventDT(1, 'Item A', "2017-03-06T09:00", "2017-03-06T11:00") );
-    this.newEvent( new EventDT(4, 'Item D', "2017-03-06T13:30", "2017-03-06T17:45") );
-    this.newEvent( new EventDT(2, 'Item B', "2017-03-08T13:00", "2017-03-08T17:00") );
-    this.newEvent( new EventDT(3, 'Item C', "2017-03-18T09:00", "2017-03-18T11:00") );
-  }
 };
